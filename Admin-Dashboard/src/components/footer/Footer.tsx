5b70af550af699afffb6979313ea17d0be599749
Manas Shinde <139111725+manashinde16@users.noreply.github.com>
2023-10-27 18:43:46 +0530

    files

---
+++ Admin-Dashboard/src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Manashinde</span>
+      <span>©Manashinde Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
